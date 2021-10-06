332342aa2e728a13bd3d2640d30bcc70e3cc38ea
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-10-06 16:01:36 +0530

    recycler view

--- app/src/main/java/com/kriti/android1month/RecyclerDemo.java
+++ app/src/main/java/com/kriti/android1month/RecyclerDemo.java
@@ -2,15 +2,19 @@ package com.kriti.android1month;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RecyclerDemo extends AppCompatActivity {
 
-    RecyclerView recyclerView;
+    RecyclerView recyclerView; MyAdapter adapter;
     String[] text = {"One", "Two", "Three", "Four", "Five", "Six", "Seven"};
     int[] images = {R.drawable.one, R.drawable.two, R.drawable.three, R.drawable.four,
             R.drawable.five, R.drawable.six, R.drawable.seven};
@@ -21,6 +25,11 @@ public class RecyclerDemo extends AppCompatActivity {
         setContentView(R.layout.activity_recycler_demo);
 
         recyclerView = findViewById(R.id.recyclerView);
+        adapter = new MyAdapter();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
     }
 
     class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>
@@ -28,25 +37,38 @@ public class RecyclerDemo extends AppCompatActivity {
 
         class MyViewHolder extends RecyclerView.ViewHolder
         {
-            public MyViewHolder(@NonNull View itemView) {
+            ImageView imageView;
+            TextView textView;
+
+            public MyViewHolder(@NonNull View itemView)
+            {
                 super(itemView);
+                imageView = itemView.findViewById(R.id.customImageView);
+                textView = itemView.findViewById(R.id.customTextView);
             }
         }
 
         @NonNull
         @Override
         public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+            LayoutInflater inflater = LayoutInflater.from(RecyclerDemo.this);
+            View view = inflater.inflate(R.layout.custom_layout, parent, false);
+            return new MyAdapter.MyViewHolder(view);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
+        public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position)
+        {
+            int displayImage = images[position];
+            String displayText = text[position];
 
+            holder.textView.setText(displayText);
+            holder.imageView.setImageResource(displayImage);
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return text.length;
         }
     }
 }
